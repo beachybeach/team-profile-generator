e8899a6febd4ed57466371db55cd774413aa6d9d
Riley <rsbeach97@gmail.com>
2020-12-06 17:25:27 -0700

    creates manager

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const inquirer = require("inquirer");
 
+const Manager = require("./lib/Manager");
+
 const run = async () => {
-  const answers = await inquirer.prompt([
-    { name: "name", message: "What is your name?" },
+  const { name, id, email, office } = await inquirer.prompt([
+    { name: "name", message: "What is the team managers name?" },
+    { name: "id", message: "What is the team managers ID number?" },
+    { name: "email", message: "What is the team managers email?" },
+    { name: "office", message: "What is the team managers office number?" },
   ]);
-  console.log(answers);
+
+  const manager = new Manager(name, id, email, office);
+
+  console.log(manager);
 };
 
 run();
