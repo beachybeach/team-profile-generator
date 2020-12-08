6c44857577e0540fa5f3cff9f9304581510ee08f
Riley <rsbeach97@gmail.com>
2020-12-07 20:19:44 -0700

    constructors for team members made

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 
 const managerInfo = async () => {
-  const { name, id, email, office } = await inquirer.prompt([
+  const { name, id, email, office, team } = await inquirer.prompt([
     { name: "name", message: "What is the team managers name?" },
     { name: "id", message: "What is the team managers ID number?" },
     { name: "email", message: "What is the team managers email?" },
     { name: "office", message: "What is the team managers office number?" },
+    {
+      type: "list",
+      message: "What type of team member would you like to add?",
+      name: "team",
+      choices: ["Engineer", "Intern", "I don't want to add any more members."],
+    },
   ]);
 
   const manager = new Manager(name, id, email, office);
-
+  console.log(team);
   console.log(manager);
 };
 
 managerInfo();
+
+if (team !== "I don't want to add any more members.") {
+}
+
+if (team === "I don't want to add any more members.") {
+  //creates html w/ team members chosen
+}
