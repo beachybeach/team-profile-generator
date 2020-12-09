8a18c77a1684ebef762a0c45262be612a7ca4e99
Riley <rsbeach97@gmail.com>
2020-12-08 20:19:48 -0700

    created separate team file to build team

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require("inquirer");
 
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
 
 const createEngineer = () => {
   // return  { name, id, email, github }
