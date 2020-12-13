1f4d06956c4a781081ddffaa6ccee0651d57dc99
Riley <rsbeach97@gmail.com>
2020-12-13 10:58:08 -0700

    styled okay I guess.....

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# team-profile-generator
+# Team Profile Generatore
+
+## [Application Link](https://beachybeach.github.io/team-profile-generator/)
+
+## Screenshot
+
+<img width="500" src="./src/images/screenshot.png">
+
+This "coding quiz" is a quiz that is DEFINITELY about coding related questions and totally not about horror movies.
+
+This is a basic quiz with one question, four answers following, and only 60 seconds to complete. Any incorrect answers will then deduct 5 seconds from the remaining time. Your final score is then shown on the screen and put into local storage so you can see how much smarter you are than your peers.
+
+# Uses
+
+To have a great time and buff up on your murder movie...I mean <strong><em>coding knowledge.</em></strong>
+
+# Built With
+
+- HTML
+- CSS
+- Javascript
+- Blood, sweat, tears, and <em>lots,</em> more blood.
--- src/template.js
+++ src/template.js
@@ -11,17 +11,49 @@ const buildPage = (teamMembers) => {
   //CONSTRUCTS THE HTML
   const createMemberHTML = ({ name, id, email, member, ...rest }) => {
     const extra = Object.values(rest);
-    return `
+
+    if (member === "Manager") {
+      return `
     <div class="card col text-center" style="width: 18rem;">
     <div class="card-body">
+    <i class="fas fa-mug-hot fa-lg"></i>
+    <h5 class="card-title">${member}</h5>
     <h5 class="card-title">Name: ${name}</h5>
     <p>ID Number: ${id}</p>
       <p class="card-text"><a href="mailto:${email}">Email: ${email}</a></p>
-      <p class="card-text">Position: ${member}</p>
-      <p class="card-text">${extra}</p>
+      <p class="card-text">Office Number: ${extra}</p>
       </div>
      </div>
     `;
+    } else if (member === "Engineer") {
+      return `
+    <div class="card col text-center" style="width: 18rem;">
+    <div class="card-body">
+    <i class="fas fa-glasses fa-lg"></i>
+    <h5 class="card-title">${member}</h5>
+    <h5 class="card-title">Name: ${name}</h5>
+    <p>ID Number: ${id}</p>
+      <p class="card-text"><a href="mailto:${email}">Email: ${email}</a></p>
+      <p class="card-text">Github:<a href="https://github.com/${extra}"> ${extra}</a></p>
+      </div>
+     </div>
+    `;
+    } else {
+      if (member === "Intern") {
+        return `
+    <div class="card col text-center" style="width: 18rem;">
+    <div class="card-body">
+    <i class="fas fa-user-graduate fa-lg"></i>
+    <h5 class="card-title">${member}</h5>
+    <h5 class="card-title">Name: ${name}</h5>
+    <p>ID Number: ${id}</p>
+      <p class="card-text"><a href="mailto:${email}">Email: ${email}</a></p>
+      <p class="card-text">School: ${extra}</p>
+      </div>
+     </div>
+    `;
+      }
+    }
   };
 
   const membersHTML = teamMembers.map((member) => createMemberHTML(member));
@@ -33,6 +65,12 @@ const buildPage = (teamMembers) => {
     <meta charset="utf-8">
     <title></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous"
+    />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/Minty/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="./src/style.css" rel="stylesheet" />
